80451576511fb127b34dcd81b399056f15d5b488
WolffJosephN <wolff.joseph.n@gmail.com>
2020-07-09 17:42:28 -0400

    Forgot to save postRouter

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -4,7 +4,11 @@ router.get("/", (req, res) => {
     res.send("/posts works");
 });
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
+    const { title, createdOn, tags, content } = req.body;
+    console.log(req.body);
+
+
     res.send(req.body);
 });
 
